f18a70e27aa76a9e3c04edb0ef0577219b692f06
WebGeospatial <r.hodgson@webgeospatial.co.uk>
2019-08-30 11:21:32 +0100

    updates 30/08

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,12 +30,11 @@ import { SearchboxComponent } from './searchbox/searchbox.component';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-
+    LeafletModule.forRoot(),
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests and returns simulated server responses. Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
     InMemoryDataService, { dataEncapsulation: false }
     )
-
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/selectlocation/selectlocation.component.ts
+++ src/app/selectlocation/selectlocation.component.ts
@@ -1,28 +1,89 @@
 //import core angular functions needed by this component
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 //import the data structure and type definitiion - added here to aid binding
 import { locationClassDefinition } from '../selectlocation';
 //import the service export
 import { GetlocationsService } from '../getlocations.service';
 
+//attempt at includng a leaflet module and parts
+//import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { latLng, tileLayer, circle, polygon, Map } from 'leaflet';
+import { reduce } from 'rxjs/operators';
+
 //define this components parts
 @Component({
   selector: 'app-selectlocation',
   templateUrl: './selectlocation.component.html',
-  styleUrls: ['./selectlocation.component.css']
+  styleUrls: ['./selectlocation.component.css'],
 })
 
 //export oninitialisation the following
 export class SelectlocationComponent implements OnInit {
   //defines the data structure and type definitiion of variable userlocations
   userlocations: locationClassDefinition[];
+  //define other variables with type
+  locationMessage : string;
+  Lat : number;
+  Lon : number;
+  options;
+  layersControl;
+  layers;
+  marker;
+  clickedCoords;
+  browserCoords;
   
-  //constructs a private instance of the service for use in this component only
-  constructor(private PrivateInstanceOfService: GetlocationsService) { }
-  
-  //calls LoadLocations() from this component 
+
+  constructor(
+    //constructs a private instance of the service for use in this component only
+    private PrivateInstanceOfService: GetlocationsService
+  ) {}
+
+  //calls methods from this component 
   ngOnInit() {
     this.LoadLocations();
+    this.setupMap();
+  }
+
+  //registering a mapready event
+  onMapReady(map: Map) : void {
+    // Onclick set the location variables
+    map.on('click', () => {
+      this.clickedCoords = map.mouseEventToLatLng(event);
+      this.Lat = this.clickedCoords.lat;
+      this.Lon = this.clickedCoords.lng;
+      this.clickedCoords = 'LatLng {lat:' + this.Lat + ',lng:' + this.Lon + '}';
+      this.layers = [
+        circle([ this.Lat, this.Lon ], { radius: 10000 , color: 'red'})
+      ];
+    });
+  }
+
+  
+  //setupmap
+  setupMap() : void { 
+
+  //create map options
+  this.options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      })
+    ],
+    zoom: 7,
+    center: latLng([ 5.3, 6.4 ])
+  }
+
+  //add extra map features
+  this.layersControl = {
+    baseLayers: {
+      'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+      'Open Cycle Map': tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    },
+    overlays: {
+      //'Big Circle': circle([ 5.3, 6.4 ], { radius: 10000 })
+    }
+    
+  }
   }
 
   //internal component function to assign the data made available from the private service instance to this.userlocations
@@ -43,9 +104,29 @@ export class SelectlocationComponent implements OnInit {
       });
   }
 
+  //get location from the browser - likely needs moving to a seperate service
+  getBrowserLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.Lat = position.coords.latitude;
+        this.Lon = position.coords.longitude;
+        this.browserCoords = 'LatLng {lat:' + position.coords.latitude + ',lng:' + position.coords.longitude + '}';
+        //this.updatemessage("Browser geoloction returned coordinates");
+      });
+    } else { 
+      this.updatemessage("Browser geolocation is not supported"); 
+    }
+  }
+
+  //function to delete an item
   delete(userlocations: locationClassDefinition): void {
     this.userlocations = this.userlocations.filter(h => h !== userlocations);
     this.PrivateInstanceOfService.deleteItem(userlocations).subscribe();
+  }
+
+  //function to update message
+  updatemessage(locationMessage : string): void {
+    this.locationMessage = locationMessage;
   }
   
 
